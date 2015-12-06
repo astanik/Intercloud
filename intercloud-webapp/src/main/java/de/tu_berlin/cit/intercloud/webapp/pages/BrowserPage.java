ef35fa1b94c02a711b6101590c84d5f2fe6e19ba
Robert Oberbeck <robert.oberbeck@campus.tu-berlin.de>
2015-12-06 21:25:33 +0100

    use alert panel for better logging (stacktarce + message)

--- intercloud-webapp/src/main/java/de/tu_berlin/cit/intercloud/webapp/pages/BrowserPage.java
+++ intercloud-webapp/src/main/java/de/tu_berlin/cit/intercloud/webapp/pages/BrowserPage.java
@@ -2,6 +2,7 @@ package de.tu_berlin.cit.intercloud.webapp.pages;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.block.Code;
 import de.agilecoders.wicket.core.markup.html.bootstrap.block.CodeBehavior;
+import de.agilecoders.wicket.core.markup.html.bootstrap.dialog.Alert;
 import de.tu_berlin.cit.intercloud.client.model.rest.AbstractRepresentationModel;
 import de.tu_berlin.cit.intercloud.client.model.rest.MethodModel;
 import de.tu_berlin.cit.intercloud.client.model.rest.OcciRepresentationModel;
@@ -9,10 +10,12 @@ import de.tu_berlin.cit.intercloud.client.model.rest.TextRepresentationModel;
 import de.tu_berlin.cit.intercloud.client.model.rest.UriListRepresentationModel;
 import de.tu_berlin.cit.intercloud.client.service.IIntercloudClient;
 import de.tu_berlin.cit.intercloud.webapp.IntercloudWebSession;
+import de.tu_berlin.cit.intercloud.webapp.components.ComponentUtils;
 import de.tu_berlin.cit.intercloud.webapp.panels.OcciRequestPanel;
 import de.tu_berlin.cit.intercloud.webapp.panels.UriResponsePanel;
 import de.tu_berlin.cit.intercloud.webapp.template.UserTemplate;
 import de.tu_berlin.cit.intercloud.xmpp.rest.XmppURI;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.behavior.AttributeAppender;
@@ -31,7 +34,6 @@ import org.apache.wicket.model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BrowserPage extends UserTemplate {
@@ -41,33 +43,32 @@ public class BrowserPage extends UserTemplate {
 
     private final IModel<String> entity;
     // ajax components
-    private final Code codePanel;
     private final MethodTable methodTable;
+    private final Alert alert;
+    private final Code xwadlCode;
+    private final Code restCode;
 
     // serialize (relevant for browser history)
     private IModel<String> resourcePath;
 
     // don't serialize (not relevant for browser history)
-    private transient String codeString;
-    private transient List<MethodModel> methodModelList = new ArrayList<>();
+    private transient List<MethodModel> methodModelList; //= new ArrayList<>();
 
     public BrowserPage(IModel<XmppURI> uri) {
         super();
         this.entity = new Model<>(uri.getObject().getJID());
         this.resourcePath = new Model<>(uri.getObject().getPath());
 
+        // alert
+        this.alert = newAlert("alert");
+        this.add(this.alert);
         this.add(new XwadlForm("xwadlForm"));
 
         // Code section, kind of debugging
-        this.codePanel = new Code("code", new LoadableDetachableModel<String>() {
-            @Override
-            protected String load() {
-                return codeString;
-            }
-        });
-        this.codePanel.setLanguage(CodeBehavior.Language.XML);
-        this.codePanel.setOutputMarkupId(true);
-        this.add(this.codePanel);
+        this.xwadlCode = newCodePanel("xwadlCode");
+        this.add(this.xwadlCode);
+        this.restCode = newCodePanel("restCode");
+        this.add(this.restCode);
 
         // method table
         this.methodTable = new MethodTable("methodTable", new LoadableDetachableModel<List<MethodModel>>() {
@@ -82,6 +83,50 @@ public class BrowserPage extends UserTemplate {
         // request / response
         this.add(new EmptyPanel(ID_REQUEST_PANEL));
         this.add(new EmptyPanel(ID_RESPONSE_PANEL));
+
+        // request xwadl
+        if (!resourcePath.getObject().trim().isEmpty()) {
+            requestXwadl(entity.getObject(), resourcePath.getObject(), null);
+        }
+    }
+
+    private Alert newAlert(String markupId) {
+        Alert alert = new Alert(markupId, Model.of(), Model.of());
+        alert.type(Alert.Type.Danger);
+        alert.setOutputMarkupId(true);
+        return ComponentUtils.displayNone(alert);
+    }
+
+    private Alert logAlert(Throwable t) {
+        alert.withHeader(Model.of(ExceptionUtils.getMessage(t)));
+        alert.withMessage(Model.of(ExceptionUtils.getStackTrace(t)));
+        return ComponentUtils.displayBlock(alert);
+    }
+
+    private Code newCodePanel(String markupId) {
+        Code code = new Code(markupId);
+        code.setLanguage(CodeBehavior.Language.XML);
+        code.setOutputMarkupId(true);
+        return code;
+    }
+
+    private void requestXwadl(String entity, String path, AjaxRequestTarget target) {
+        try {
+            XmppURI uri = new XmppURI(entity, path);
+            IIntercloudClient intercloudClient = IntercloudWebSession.get().getIntercloudService().newIntercloudClient(uri);
+            this.xwadlCode.setDefaultModel(Model.of(intercloudClient.toString()));
+            this.methodModelList = intercloudClient.getMethods();
+            ComponentUtils.displayNone(this.alert);
+        } catch (Exception e) {
+            logger.error("Failed to request xwadl from entity: {}, path: {}", entity, path);
+            this.xwadlCode.setDefaultModel(Model.of());
+            this.methodModelList = null;
+            logAlert(e);
+        }
+
+        if (null != target) {
+            target.add(this.xwadlCode, this.methodTable, this.alert);
+        }
     }
 
     private class XwadlForm extends Form {
@@ -94,21 +139,7 @@ public class BrowserPage extends UserTemplate {
             AjaxButton button = new AjaxButton("getXwadlBtn") {
                 @Override
                 protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                    try {
-                        XmppURI uri = new XmppURI(entity.getObject(), resourcePath.getObject());
-                        IIntercloudClient intercloudClient = IntercloudWebSession.get().getIntercloudService()
-                                .newIntercloudClient(uri);
-                        codeString = intercloudClient.toString();
-                        methodModelList = intercloudClient.getMethods();
-                        target.add(codePanel);
-                        target.add(methodTable);
-                    } catch (Exception e) {
-                        StringBuilder s = new StringBuilder();
-                        s.append("Failed to receive xwadl from xmpp://").append(entity.getObject()).append("#").append(resourcePath.getObject()).append(".");
-                        logger.error(s.toString(), e);
-                        target.appendJavaScript(s.insert(0, "alert('").append("');").toString());
-                        return;
-                    }
+                    BrowserPage.this.requestXwadl(entity.getObject(), resourcePath.getObject(), target);
                 }
             };
             this.add(button);
@@ -151,13 +182,15 @@ public class BrowserPage extends UserTemplate {
                                     .getIntercloudClient(methodModel.getUri())
                                     .executeMethod(null, methodModel);
                             if (representation instanceof UriListRepresentationModel) {
-                                codeString = null;
+                                restCode.setDefaultModel(new Model<>());
                                 BrowserPage.this.replace(new UriResponsePanel(ID_RESPONSE_PANEL, new Model<>((UriListRepresentationModel) representation)));
                             } else if (representation instanceof TextRepresentationModel) {
-                                codeString = ((TextRepresentationModel) representation).getText();
+                                restCode.setDefaultModel(new Model<>(((TextRepresentationModel) representation).getText()));
                             }
                         }
+                        ComponentUtils.displayNone(alert);
                     } catch (Exception e) {
+                        logAlert(e);
                         logger.error("Failed to execute request. {}", methodModel, e);
                     }
 
@@ -170,7 +203,9 @@ public class BrowserPage extends UserTemplate {
                                     new Model<>(methodModel),
                                     new Model<>((OcciRepresentationModel) representationModel)));
                         }
+                        ComponentUtils.displayNone(alert);
                     } catch (Exception e) {
+                        logAlert(e);
                         logger.error("Could not create occi request model.", e);
                     }
                 }
